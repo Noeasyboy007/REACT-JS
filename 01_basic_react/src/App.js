301c8bef256ae26170c4546a82ca4af617a21815
Aritra Bera <aritrabera69@gmail.com>
2024-05-25 17:55:14 +0530

    01_basic_react

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,12 @@
+import Aritra from "./Aritra";
+function App() {
+  return (
+    <>
+    <Aritra></Aritra>
+    {/* <h1>Aritra Bera</h1> */}
+    </>
+  );
+}
+
+export default App;
+
---
+++ 01_basic_react/src/Aritra.js
@@ -0,0 +1,7 @@
+function Aritra(){
+    return(
+        <h2>ARITRA BERA ARITRA BERA</h2>
+    )
+}
+
+export default Aritra;
